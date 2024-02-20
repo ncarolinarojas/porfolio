515acf258152a0d8ebda4a0f39eaa73d20dd7af0
ncarolinarojas <nataliarojasco22@gmail.com>
2024-02-20 10:20:56 -0500

    start

---
+++ porfolioNataliaDev/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path='/home'> Hola </Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App
